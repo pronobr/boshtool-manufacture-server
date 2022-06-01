89be28fcdc5fbcad7a060ca22d8237432e5e0226
pronobroy <pronobroy3601@gmail.com>
2022-06-01 07:05:40 -0700

    push

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var ObjectId = require('mongodb').ObjectID;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
-
+// pronob
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6nzo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
